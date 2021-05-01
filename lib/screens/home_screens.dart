5dfb4b4f1738a92c909965ea625129717459e0d6
Alexander <38163966+KislyakovS@users.noreply.github.com>
2021-05-01 09:48:04 +0300

    Added home screen button

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,3 +1,4 @@
+import 'package:clubhouse_clone/config/palette.dart';
 import 'package:clubhouse_clone/data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -54,14 +55,81 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
-      body: ListView(
-        padding: const EdgeInsets.fromLTRB(20, 20, 20, 120),
+      body: Stack(
+        alignment: Alignment.center,
         children: [
-          UpcomingRooms(upcomingRooms: upcomingRoomsList),
-          const SizedBox(
-            height: 12,
+          ListView(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 120),
+            children: [
+              UpcomingRooms(upcomingRooms: upcomingRoomsList),
+              const SizedBox(
+                height: 12,
+              ),
+              ...roomsList.map((e) => RoomCard(room: e))
+            ],
           ),
-          ...roomsList.map((e) => RoomCard(room: e))
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 100,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.1),
+                    Theme.of(context).scaffoldBackgroundColor
+                  ])),
+            ),
+          ),
+          Positioned(
+            bottom: 60,
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.circular(24)),
+              child: const Text.rich(TextSpan(children: [
+                WidgetSpan(
+                    child: Icon(CupertinoIcons.add,
+                        size: 21, color: Colors.white)),
+                TextSpan(
+                    text: ' Start room',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500))
+              ])),
+            ),
+          ),
+          Positioned(
+            bottom: 60,
+            right: 40,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                IconButton(
+                    icon: const Icon(
+                      CupertinoIcons.circle_grid_3x3_fill,
+                      color: Colors.black,
+                    ),
+                    onPressed: null),
+                Positioned(
+                  right: 5.6,
+                  bottom: 10.8,
+                  child: Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Theme.of(context).accentColor),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
